1d5a58dba4843ee46b6867583fc7e2b508f29edc
TurqaySafarov <161557826+TurqaySafarov@users.noreply.github.com>
2024-05-03 15:31:06 +0400

    first commit

---
+++ python_task1.py
@@ -0,0 +1,39 @@
+# Userden alinan 5 cavabi bir listde sirali seklinde yerlesdirin:
+
+# list=[]
+# for i in range(1,6):
+#     num=int(input("Eded daxil edin: "))
+#     list.append(num)
+# list.sort()
+# print(list)    
+
+
+# Daxil edilmiş cümlənin bütün sözlərini alfabetik düzülüşlü sözlərə çevirib nəticəni çap edin.
+
+# word=input("Bir ifade daxil edin: ")
+# list=list(word)
+# list.sort()
+# print(list)
+
+
+#n dəyərini təxmin etmək
+
+# n = 14
+# num = 0
+# k = 0
+# while n != num:
+#     num = int(input("n deyerini tapmaga calisin: "))
+#     k = k+1
+# print(f"tebrikler n deyerini {k} cehdde tapdiniz")
+
+
+# 1 ile 100 arasinda sade ededleri print edin. (for loops)
+
+# for i in range(3, 100):
+#     sade = True
+#     for j in range(2, int(i**0.5) + 1):
+#         if i % j == 0:
+#             sade = False
+#             break
+#     if sade:
+#         print(i)
